88fe169f282dd9d09f0ff5fe8a63825cc483a0d9
kosne1 <akolia2017@list.ru>
2025-05-10 13:42:26 +0300

    feat: add bot running

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ import asyncio
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-# from app import bot
+from app import bot
 
 # import app.handlers
 scheduler = AsyncIOScheduler()
@@ -13,8 +13,11 @@ def example_function():
 
 
 async def main():
-    scheduler.add_job(example_function, 'cron', hour=13, minute=28)
+    scheduler.add_job(example_function, 'cron', hour=1, minute="35, 36, 37")
     scheduler.start()
+
+    await bot.polling(none_stop=True)
+
     await asyncio.Event().wait()
 
 
